0dc01dfbe6e2998530d05f8a2433e8495904d1d4
Robert Nguyen <robertng0407@gmail.com>
2017-06-06 22:06:01 -0700

    Add edit update functionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the *.GLOB, #{@user.username}"
-      redirect_to articles_path(@user)
+      redirect_to articles_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
