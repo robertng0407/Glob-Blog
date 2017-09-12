8b08750f8de91b9660c415681d6744412ede17e2
Robert Nguyen <robertng0407@gmail.com>
2017-09-11 21:40:38 -0700

    Add config folder

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :articles
+  resources :users, except: [:new]
+end
